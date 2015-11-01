19ac297085edafed580830f1bcbf3b09eacecbcd
dadd2 <f.fifov@yandex.ru>
2015-11-01 13:57:07 +0300

    Create consts.py

---
+++ consts.py
@@ -0,0 +1,10 @@
+WEEK = 'пн вт ср чт пт сб'.split()
+HOURS_IN_DAY = 5
+DAYS_COUNT = 3
+
+#SLEEP_TIMES = [0.2 / (2 ** i) for i in range(1, 8)]
+SLEEP_TIMES = [0.05] + [0 for i in range(1, 7)]
+##SLEEP_TIMES = [0.2, 0.1, 0.05, 0.025, 0.0125, 0.00625, 0.003125]
+C_W = 50
+C_H = 50
+PAL = 'red orange yellow #0f0 lightblue blue violet'.split()
